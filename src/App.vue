3f4765085fa38fb8c3c15c83a7243ceeb9175cfb
Rodrigo Ribeiro <rodrigodepaula1989@gmail.com>
2022-08-30 19:52:01 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <Navbar :logo="logo_vue" :alt="name_app" />
+  <router-view />
+  <Footer />
+</template>
+
+<script>
+import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
+
+export default {
+  components: {
+    Navbar,
+    Footer,
+  },
+  data() {
+    return {
+      logo_vue: "/img/logo.png",
+      name_app: "NHAM NHAM Burger",
+    };
+  },
+};
+</script>
+<style>
+*{
+  font-family: Arial, Helvetica, sans-serif;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.main-container {
+  margin: 50px;
+  min-height: 250px;
+}
+
+h1 {
+  text-align: center;
+  font-size: 42px;
+  margin-bottom: 30px;
+  color: #222;
+
+}
+</style>
